94f8bcd472ca10e94fcead6c18cc6f245ec6547a
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-03-14 15:35:22 +0200

    Testing webpack and p3

--- src/main/java/org/vaadin/artur/gamecard/GameCard.java
+++ src/main/java/org/vaadin/artur/gamecard/GameCard.java
@@ -2,10 +2,10 @@ package org.vaadin.artur.gamecard;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JsModule;
 
 @Tag("game-card")
-@HtmlImport("bower_components/game-card/game-card.html")
+@JsModule("./game-card.js")
 public class GameCard extends Component {
 
     public GameCard() {
