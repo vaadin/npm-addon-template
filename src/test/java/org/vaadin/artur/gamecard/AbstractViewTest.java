fd87171d4c9cf1b36007a77a0788a196caab94f4
Johannes Eriksson <joheriks@vaadin.com>
2019-05-27 11:04:09 +0300

    Removed unused test base class methods, Testbenchified test

--- src/test/java/org/vaadin/artur/gamecard/AbstractViewTest.java
+++ src/test/java/org/vaadin/artur/gamecard/AbstractViewTest.java
@@ -1,13 +1,7 @@
 package org.vaadin.artur.gamecard;
 
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.vaadin.testbench.ScreenshotOnFailureRule;
@@ -32,19 +26,17 @@ public abstract class AbstractViewTest extends ParallelTest {
     private static final int SERVER_PORT = 8080;
 
     private final String route;
-    private final By rootSelector;
 
     @Rule
     public ScreenshotOnFailureRule rule = new ScreenshotOnFailureRule(this,
             true);
 
     public AbstractViewTest() {
-        this("", By.tagName("body"));
+        this("");
     }
 
-    protected AbstractViewTest(String route, By rootSelector) {
+    protected AbstractViewTest(String route) {
         this.route = route;
-        this.rootSelector = rootSelector;
     }
 
     @Before
@@ -57,21 +49,6 @@ public abstract class AbstractViewTest extends ParallelTest {
         getDriver().get(getURL(route));
     }
 
-    @After
-    public void finalCheck() {
-        getDriver().quit();
-    }
-
-    /**
-     * Convenience method for getting the root element of the view based on
-     * the selector passed to the constructor.
-     *
-     * @return the root element
-     */
-    protected WebElement getRootElement() {
-        return findElement(rootSelector);
-    }
-
     /**
      * Returns deployment host name concatenated with route.
      *
@@ -80,25 +57,6 @@ public abstract class AbstractViewTest extends ParallelTest {
     private static String getURL(String route) {
         return String.format("http://%s:%d/%s", getDeploymentHostname(),
                 SERVER_PORT, route);
-    }
-
-    /**
-     * Finds elements by selector under shadow root of given web component.
-     *
-     * @param webComponent - component root
-     * @return result of selection
-     */
-    protected List<WebElement> findInShadowRoot(WebElement webComponent,
-                                                By by) {
-
-        waitUntil(driver -> getCommandExecutor().executeScript(
-                "return arguments[0].shadowRoot", webComponent) != null);
-        WebElement shadowRoot = (WebElement) getCommandExecutor()
-                .executeScript("return arguments[0].shadowRoot",
-                        webComponent);
-        Assert.assertNotNull("Could not locate shadowRoot in the element",
-                shadowRoot);
-        return shadowRoot.findElements(by);
     }
 
     /**
--- src/test/java/org/vaadin/artur/gamecard/DemoViewIT.java
+++ src/test/java/org/vaadin/artur/gamecard/DemoViewIT.java
@@ -2,18 +2,15 @@ package org.vaadin.artur.gamecard;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.vaadin.testbench.TestBenchElement;
 
 public class DemoViewIT extends AbstractViewTest {
 
     @Test
     public void testIt() {
-        waitUntil(ExpectedConditions.presenceOfElementLocated(
-                By.tagName("game-card")));
-        final WebElement gameCard = findElement(By.tagName("game-card"));
-        Assert.assertEquals("♥", gameCard.getAttribute("symbol"));
-        Assert.assertEquals("A", gameCard.getAttribute("rank"));
+        final TestBenchElement card = $("game-card").first();
+        Assert.assertEquals("♥", card.getAttribute("symbol"));
+        Assert.assertEquals("A", card.getAttribute("rank"));
     }
 }
