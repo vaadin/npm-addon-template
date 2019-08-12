a33c947838d838dacc8f08cca6b9aed11aa310e8
Johannes Eriksson <joheriks@vaadin.com>
2019-08-12 11:11:06 +0300

    Hint how to include JavaScript module also in .java file (#119)

--- src/main/java/org/vaadin/artur/paperslider/PaperSlider.java
+++ src/main/java/org/vaadin/artur/paperslider/PaperSlider.java
@@ -8,6 +8,11 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 @Tag("paper-slider")
 @JsModule("@polymer/paper-slider/paper-slider.js")
 @NpmPackage(value = "@polymer/paper-slider", version = "^3.0.1")
+/*
+ If you wish to include your own JS modules in the add-on jar, add the module
+ files to './src/main/resources/META-INF/resources/frontend' and insert an
+ annotation @JsModule("./my-module.js") here.
+*/
 public class PaperSlider extends Component {
 
     public PaperSlider() {
