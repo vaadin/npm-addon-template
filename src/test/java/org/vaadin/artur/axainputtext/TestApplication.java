705b7eee2ae771ffad261280582941d87b92150a
Matti Tahvonen <matti@vaadin.com>
2022-10-12 17:15:56 +0300

    cleanup & comment

--- src/test/java/org/vaadin/artur/axainputtext/TestApplication.java
+++ src/test/java/org/vaadin/artur/axainputtext/TestApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
+    /**
+     * Run this method to start a test server during development iterations.
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
